9efaa5ea1ffc325dadd1e13d5277de716e277054
Abdellah Aghzal <abdellahaghzal@hotmail.com>
2026-02-22 10:43:49 +0000

    using each component in its own file

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,37 +1,5 @@
 import { useState } from 'react'
-
-const StatisticLine = ({text, value}) => {
-    return (
-        <tr>
-            <td>{text}</td>
-            <td>{value}</td>
-        </tr>
-    )
-}
-
-const Statistics = ({good, neutral, bad}) => {
-    const all = good + neutral + bad
-    if (all === 0) {
-        return (
-            <div>No feedback given</div>
-        )
-    }
-
-    const average = (good - bad) / all
-    const positive = good / all * 100
-    return (
-        <table>
-            <tbody>
-                <StatisticLine text="good" value={good}/>
-                <StatisticLine text="neutral" value={neutral}/>
-                <StatisticLine text="bad" value={bad}/>
-                <StatisticLine text="all" value={all}/>
-                <StatisticLine text="average" value={average}/>
-                <StatisticLine text="positive" value={positive + " %"}/>
-            </tbody>
-        </table>
-    )
-}
+import Statistics from './Statistics'
 
 const App = () => {
   const [good, setGood] = useState(0)
---
+++ part1/unicafe/src/Statistics.jsx
@@ -0,0 +1,27 @@
+import StatisticLine from "./StatisticLine"
+
+const Statistics = ({good, neutral, bad}) => {
+    const all = good + neutral + bad
+    if (all === 0) {
+        return (
+            <div>No feedback given</div>
+        )
+    }
+
+    const average = (good - bad) / all
+    const positive = good / all * 100
+    return (
+        <table>
+            <tbody>
+                <StatisticLine text="good" value={good}/>
+                <StatisticLine text="neutral" value={neutral}/>
+                <StatisticLine text="bad" value={bad}/>
+                <StatisticLine text="all" value={all}/>
+                <StatisticLine text="average" value={average}/>
+                <StatisticLine text="positive" value={positive + " %"}/>
+            </tbody>
+        </table>
+    )
+}
+
+export default Statistics
